8e90d6163e09b9c488ec7611d7b613dfec94145c
misosoup17 <misagaspar@icloud.com>
2019-08-06 20:35:10 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,3 @@ var loses = 0
 
 var animal = animals[Math.floor(Math.random() * animals.length)];
 
-
-var remainingLetters = animal.length;
-var  (var i=0, wordToMatch.length; i < ; i++){
-
-  var (wordToMatch[i] === " ") {
-      guessingWord.push(" ")
-  } else {
-      guessingWord.push("_")
