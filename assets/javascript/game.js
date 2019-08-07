6e5bef40cdc1ad69257deed77222ce26ff464e58
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-08-06 21:13:57 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
 var animals = ["wombat","rhino","cheetah","tiger","koala"]
 var wins = 0;
 var loses = 0;
+var guess = "";
+
 
 var blanks = [];
 var randomAnimals = ""
 
+// genanerating random animal
 var animal = animals[Math.floor(Math.random() * animals.length)];
 
-for (var i = 0; i < animals.length; i++) {
+console.log(animal)
+// capture the keyStroke 
+
+
+// display the letters
+
+// looping though the animal to get the correct letters
+for (var i = 0; i < animal.length; i++) {
 
 }
